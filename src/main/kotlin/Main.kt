cc36b12584aa67264a9c2003bd246998a13f3595
rpanic <raphael.panic@gmx.at>
2020-01-11 22:08:03 +0100

    Fixed Parameters

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -55,7 +55,7 @@ class Main{
                     devPath = "src/main/resources/"
 
                 }
-            }else if(args[i].startsWith("-s")) {   //-ssl
+            }else if(args[i].startsWith("-ss")) {   //-ssl
 
                 ssl = args[i + 1].toBoolean()
 
